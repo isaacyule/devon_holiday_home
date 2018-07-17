9e1806d74c7364e078c695dcd27c4d8743b42b56
isaac yule <isaac.k.yule@gmail.com>
2018-07-17 14:14:31 +0100

    called method in app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  after_filter :cors_set_access_control_headers
+
   def cors_set_access_control_headers
     headers['Access-Control-Allow-Origin'] = '*'
     headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
